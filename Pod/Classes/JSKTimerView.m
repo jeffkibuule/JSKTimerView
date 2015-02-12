f23c47726ad0b3ca50630d910ce9bcaa938d20dc
Joefrey Kibuule <jeff.kibuule@outlook.com>
2015-02-11 23:37:50 -0600

    Remove unnecessary dispatch_after

--- Pod/Classes/JSKTimerView.m
+++ Pod/Classes/JSKTimerView.m
@@ -133,10 +133,8 @@ static NSString *jsk_progressAnimationKey = @"progressAnimationKey";
     
     [self pauseTimer];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.timerProgressColor = self.timerProgressFinishedColor;
-    });
-    
+    self.timerProgressColor = self.timerProgressFinishedColor;
+   
     [self updateLabel];
     [self updateProgress];
     [self setNeedsDisplay];
